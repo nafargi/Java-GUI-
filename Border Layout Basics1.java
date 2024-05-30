88e7b2cff67c672ed207d6573821aba80ad88cac
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2024-05-31 00:20:38 +0300

    Create Border Layout Basics1.java

---
+++ Border Layout Basics1.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Border Layout");
+        frame.setLayout(new BorderLayout());
+
+        JButton northButton = new JButton("north");
+        JButton westButton = new JButton("west");
+        JButton eastButton = new JButton("east");
+        JButton southButton = new JButton("south");
+        JButton centerButton = new JButton("center");
+
+        frame.add(northButton,BorderLayout.NORTH);
+        frame.add(westButton,BorderLayout.WEST);
+        frame.add(eastButton,BorderLayout.EAST);
+        frame.add(southButton,BorderLayout.SOUTH);
+        frame.add(centerButton,BorderLayout.CENTER);
+
+        frame.setSize(300,400);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        System.out.println("Hello world!");
+    }
+}

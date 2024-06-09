84cc2866f4fe690804f110dc5041f7460cae6002
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2024-06-10 01:23:41 +0300

    Update Mouse Click Counter.java

--- Mouse Click Counter.java
+++ Mouse Click Counter.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-wwwwwwww
+wwwwww
 public class MousecClickCount{
     public static void main(String[] args){
         JFrame frame = new JFrame("Mouse Click Counter");

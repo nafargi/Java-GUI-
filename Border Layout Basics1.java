c12cf3743e91ba1a596ca76c28ac301e3f5e8baa
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2024-06-11 23:26:36 +0300

    Update Border Layout Basics1.java

--- Border Layout Basics1.java
+++ Border Layout Basics1.java
@@ -3,7 +3,6 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        
         JFrame frame = new JFrame("Border Layout");
         frame.setLayout(new BorderLayout());
         JButton northButton = new JButton("north");

1f7247f9495003de1f0822a073c5670d574ec280
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2024-07-02 21:54:24 +0300

    Update Dynamic Label Update Based on Text Input.java

--- Dynamic Label Update Based on Text Input.java
+++ Dynamic Label Update Based on Text Input.java
@@ -13,7 +13,7 @@ public class DocumentListenerInstance {
         JTextField textFromUser = new JTextField(20);
         JLabel label = new JLabel("Type something...");
 
-jkkkkkk
+jkkkkk
         textFromUser.getDocument().addDocumentListener(new DocumentListener() {
             public void insertUpdate(DocumentEvent e) {
                 label.setText(textFromUser.getText());
